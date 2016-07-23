5e8c839435716d32c1023319c1cfb4378b326d25
XeryusTC <armadillo@onenetbeyond.org>
2016-07-23 23:51:22 +0200

    Use TMDB to fetch the proper name of a series

--- filesort.py
+++ filesort.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import sys
 from syslog import syslog
-from unipath import Path, DIRS_NO_LINKS
+from unipath import Path, FILES_NO_LINKS
 import logging
 import re
+import tmdbsimple as tmdb
+import datetime
 
 ### Settings ###
+tmdb.API_KEY = '0ea9d4136175cd4c4ce3ad6d4a7c40fb'
 # Target directories
 MOVIE_DIR  = Path('Movies/')
 SERIES_DIR = Path('TV Shows/')
@@ -14,8 +18,13 @@ KEEP_ORIGINAL = True # Set false to remove from download directory
 AUDIO_FILES = ('wav', 'mp3', 'flac', 'ogg', 'm4a')
 VIDEO_FILES = ('mkv', 'avi', 'mp4', 'mov', 'mpeg', 'wmv', 'flv', 'm4v')
 
-# Set up logging
-logging.basicConfig(filename='/tmp/filesort.log', level=logging.DEBUG)
+### Constants ###
+ANY_MEDIA = 0
+TV_MEDIA  = 1
+
+class MediaNotFoundInTMDBException(Exception):
+    pass
+
 
 def remove_samples(file_list):
     return [f for f in file_list if 'sample' not in f.lower()]
@@ -66,26 +75,85 @@ def sort_episode(series_name, episode, torrent_path):
             dest_file))
         copy_file(torrent_path, dest)
 
-torrent_id = sys.argv[1]
-torrent_name = sys.argv[2]
-save_path = sys.argv[3]
+def find_media(name, media=ANY_MEDIA):
+    search = tmdb.Search()
+    name = name.replace('.', ' ').strip()
+    logging.debug('Searching for "{}"'.format(name))
+    if media == ANY_MEDIA:
+        search_method = search.multi
+    elif media == TV_MEDIA:
+        search_method = search.tv
+    response = search_method(query=name)
 
-logging.info('Processing torrent {}: {} in {}'.format(torrent_id, torrent_name,
-    save_path))
+    # When there are results we can just return them
+    if search.total_results > 0:
+        logging.debug('Found {} results for the query'.format(
+            search.total_results))
+        return (search, name)
 
-# Get the name of the title
-torrent_path = Path(save_path, torrent_name)
-if torrent_path.isdir():
-    raw_name = torrent_name
-else:
-    raw_name = torrent_path.stem
+    # Otherwise start removing information, start with the date
+    logging.debug('Could not find media, removing dates first')
+    name = re.sub(r'\d{4}', '', name).strip()
+    response = search_method(query=name)
+    if search.total_results > 0:
+        logging.debug('Found {} results for the updated query {}'.format(
+            search.total_results, name))
+        return (search, name)
 
-# Test if this is a TV series
-serie_re = re.compile('([\w.]+)(?=(S\d\dE\d\d))(\2)*')
-series_match = serie_re.match(raw_name)
-if series_match:
-    series_name = series_match.group(1).replace('.', ' ').strip()
-    episode = series_match.group(2)
-    logging.info('Torrent is TV series: {}, episode {}'.format(series_name,
-        episode))
-    sort_episode(series_name, episode, torrent_path)
+    raise MediaNotFoundInTMDBException()
+
+
+def deluge(torrent_id, torrent_name, save_path):
+    # Set up logging
+    logging.basicConfig(filename='/tmp/filesort.log', level=logging.DEBUG)
+
+    logging.info('Processing torrent from deluge {}: {} in {}'.format(
+        torrent_id, torrent_name, save_path))
+
+    # Get the name of the title
+    torrent_path = Path(save_path, torrent_name)
+    if torrent_path.isdir():
+        raw_name = torrent_name
+    else:
+        raw_name = torrent_path.stem
+
+    # Test if this is a TV series
+    serie_re = re.compile(r'([\w.]+)(?=(S\d{2}E\d{2}))(\2)*')
+    series_match = serie_re.match(raw_name)
+    if series_match:
+        series_name = series_match.group(1).replace('.', ' ').strip()
+        episode = series_match.group(2)
+
+        # Get information from TMDB about the series
+        try:
+            search, name = find_media(series_name, TV_MEDIA)
+
+            # Reduce the results from the TMDB search to one final answer
+            # Use the newest series found
+            latest = search.results[0]
+            latest_date = datetime.datetime.strptime(latest['first_air_date'],
+                '%Y-%m-%d').date()
+            for result in search.results:
+                date = datetime.datetime.strptime(result['first_air_date'],
+                    '%Y-%m-%d').date()
+                if date > latest_date:
+                    latest = result
+                    latest_date = date
+            logging.debug('TMDB returned: {}, original was: {}'.format(
+                latest['name'], series_name))
+            name = latest['name']
+        except MediaNotFoundInTMDBException:
+            search = None
+            name = series_name
+
+        logging.info('Torrent is TV series: {}, episode {}'.format(name,
+                episode))
+        sort_episode(name, episode, torrent_path)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: python {} <torrent_id> <torrent_name> <torrent_path>"
+                .format(sys.argv[0]))
+        sys.exit(1)
+
+    deluge(sys.argv[1], sys.argv[2], sys.argv[3])
